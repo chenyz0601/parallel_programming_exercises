6a8fc6ecc57360473ea5e10f330bf11a8e096c91
Yuanze Chen <chenyua@in.tum.de>
2017-05-24 18:45:34 +0200

    with q_length

--- assignment2/student/histogram_par.c
+++ assignment2/student/histogram_par.c
@@ -10,8 +10,7 @@ unsigned int cur_start; // start point of each job
 unsigned int cur_end; // end point of each job
 int global_hist[N]; // for each thread to do reduction
 char *shared_buffer; // start pointer of the names
-bool get_job = true;
-bool job_created = false;
+int q_length = 0;
 bool all_done = false;
 
 pthread_mutex_t mutex_variable = PTHREAD_MUTEX_INITIALIZER; // mutex for tasks
@@ -19,25 +18,26 @@ pthread_mutex_t mutex_count = PTHREAD_MUTEX_INITIALIZER; // mutex for reduction
 
 void * Consumer(void * arg) {
     int local_hist[N]; // hold by each thread to count hist
+    bool get_my_job = false;
     // initialize
     for (int i = 0; i < N; i ++)
         local_hist[i] = 0;
     // local start and end points
     unsigned int start, end;
     while(1) {
-        bool get_my_job = false;
         // lock mutex_variable to get job
         pthread_mutex_lock(&mutex_variable);
         // set the break condition: all jobs are done, then break
         if (all_done)
             break;
-        if (job_created) {
+        if (q_length == 1) {
             start = cur_start; // get current start point
             end = cur_end;
-            get_job = true;
-            job_created = false;
             get_my_job = true;
+            q_length = 0;
         }
+        else
+            pthread_barrier_wait(0.00001ms);
         pthread_mutex_unlock(&mutex_variable);
         // count for local_hist, its job
         if (get_my_job) {
@@ -71,15 +71,14 @@ void * Producer(void * ptr) {
     while (shared_buffer[count] != TERMINATOR) {
         // create a job
         pthread_mutex_lock(&mutex_variable);
-        if (count%CHUNKSIZE == 0 && get_job) {
+        if (count%CHUNKSIZE == 0 && q_length == 0) {
             cur_start = count;
             sub_count = 1;
             while (sub_count != CHUNKSIZE || shared_buffer[cur_start+sub_count] != TERMINATOR)
                 sub_count ++;
             cur_end = cur_start + sub_count;
         }
-        job_created = true;
-        get_job = false;
+        q_length = 1;
         pthread_mutex_unlock(&mutex_variable);
         count += sub_count;
     }
--- assignment2/student/histogram_par.d
+++ assignment2/student/histogram_par.d
@@ -1,7 +1,7 @@
 student/histogram_par.o: student/histogram_par.c \
- /u/halle/chenyua/home_at/Downloads/SS16-17/ParallelProgramming/exercise/assignment2/histogram.h \
- /u/halle/chenyua/home_at/Downloads/SS16-17/ParallelProgramming/exercise/assignment2/names.h
+ /u/halle/chenyua/home_at/Downloads/SS16-17/ParallelProgramming/parallel_programming_exercises/assignment2/histogram.h \
+ /u/halle/chenyua/home_at/Downloads/SS16-17/ParallelProgramming/parallel_programming_exercises/assignment2/names.h
 
-/u/halle/chenyua/home_at/Downloads/SS16-17/ParallelProgramming/exercise/assignment2/histogram.h:
+/u/halle/chenyua/home_at/Downloads/SS16-17/ParallelProgramming/parallel_programming_exercises/assignment2/histogram.h:
 
-/u/halle/chenyua/home_at/Downloads/SS16-17/ParallelProgramming/exercise/assignment2/names.h:
+/u/halle/chenyua/home_at/Downloads/SS16-17/ParallelProgramming/parallel_programming_exercises/assignment2/names.h:
