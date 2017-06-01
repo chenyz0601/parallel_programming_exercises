7f4281b62e9e300eb78f1f1f3801700395792777
Yuanze Chen <chenyua@in.tum.de>
2017-06-01 23:57:17 +0200

    add assignment3

--- assignment3/student/histogram_par.cpp
+++ assignment3/student/histogram_par.cpp
@@ -2,38 +2,30 @@
 #include "names.h"
 
 #include <future>
-#include <thread>
 #include <vector>
 #include <algorithm>
 
-histogram_t& shared_hist;
-
-struct Result {
-    int hist[10];
-};
-
-void count_hist(promise<struct Result>&& p, vector<word_t>::iterator begin, vector<word_t>::iterator end) {
-    for_each(begin, end, [&shared_hist](const word_t word)
+histogram_t count_hist(const std::vector<word_t>& words, int b, int e) {
+    histogram_t local_hist = {{0}};
+    for_each(begin(words)+b, begin(words)+e, [&local_hist](const word_t& word)
     {
         int res = getNameIndex(word.data());
-        if (res != -1)
-            p.hist[res] ++;
+        if (res != -1) local_hist[res]++;
     });
+    return local_hist;
 }
 
-void get_histogram(const std::vector<word_t>& words, histogram_t& histogram, int num_threads)
-{
+void get_histogram(const std::vector<word_t>& words, histogram_t& histogram, int num_threads) {
     // put your code here
-    shared_hist = histogram;
-    struct Result result[num_threads];
-    for (int i = 0; i < num_threads; i ++)
-        for (int j = 0; j < 10; j ++)
-            result[i].hist[j] = 0;
-    promise<struct Result> *myPromise;
-    future<struct Result> *myFuture;
-    thread *myThread;
-    myPromise = new promise<struct Result>[num_threads];
-    myFuture = new future<struct Result>[num_threads];
-    myThread = new thread[num_threads];
-    unsigned int length = (int)words.size();
+    std::vector<std::future<histogram_t>> myFutures;
+    int chunk_size = words.size()/num_threads;
+    int res = words.size()%num_threads;
+    for (int i = 0; i < chunk_size*(num_threads-1); i += chunk_size)
+        myFutures.push_back(std::async(std::launch::async, count_hist, std::ref(words), i, i+chunk_size));
+    myFutures.push_back(std::async(std::launch::async, count_hist, std::ref(words), (num_threads-1)*chunk_size, res+num_threads*chunk_size));
+    for (auto& f : myFutures) {
+        histogram_t temp_hist = f.get();
+        for (int j = 0; j < NNAMES; j ++)
+            histogram[j] += temp_hist[j];
+    }
 }
