59b009dda49237a0a8965fb13ea1cede0491a561
Yuanze Chen <chenyua@in.tum.de>
2017-06-14 09:50:02 +0200

    add assignment5 and 6

--- assignment2/student/histogram_par.c
+++ assignment2/student/histogram_par.c
@@ -6,6 +6,63 @@
 #include <ctype.h>
 #include <unistd.h>
 #include "names.h"
+
+unsigned int last = 0, countable;
+histogram_t hist = {0};
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
+
+void * consumer(void *buffer_ptr) {
+    char *buffer = (char *)buffer_ptr;
+    histogram_t local_hist = {0};
+    unsigned int c, start, i;
+    char *b;
+    while (countable > 7483646) {
+        char current_word[20] = "";
+        c = 0;
+        pthread_mutex_lock(&mutex1);
+        start = last;
+        b = buffer+start;
+        for (i = 0; i < CHUNKSIZE; i ++) {
+            if (b[i] == TERMINATOR)
+                countable = start;
+        }
+        last += CHUNKSIZE;
+        pthread_mutex_unlock(&mutex1);
+        if (start >= countable) {
+            pthread_mutex_lock(&mutex2);
+            for (int k = 0; k < 10; k ++)
+                hist[k] += local_hist[k];
+            pthread_mutex_unlock(&mutex2);
+            return NULL;
+        }
+        for (i = 0; i < CHUNKSIZE; i ++) {
+            if (isalpha(b[i]))
+                current_word[c++] = b[i];
+            else {
+                current_word[c] = '\0';
+                int res = getNameIndex(current_word);
+                if (res != -1)
+                    local_hist[res] ++;
+                c = 0;
+            }
+        }
+    }
+    return NULL;
+}
+
+void get_histogram(char *buffer, int* histogram, int num_threads) {
+    countable = 47483647;
+    pthread_t *thread = (pthread_t*)malloc(num_threads*sizeof(*thread));
+    for (int i = 0; i < num_threads; i ++)
+        pthread_create(thread+i, NULL, &consumer, buffer);
+    for (int i = 0; i < num_threads; i ++)
+        pthread_join(thread[i], NULL);
+    for (int k = 0; k < 10; k ++)
+        histogram[k] = hist[k];
+    free(thread);
+}
+/*
 #define N 10 // number of characters
 int *global_hist; // for each thread to do reduction
 char *shared_buffer; // start pointer of the names
@@ -79,3 +136,4 @@ void get_histogram(char *buffer, int* histogram, int num_threads) {
         pthread_join(thread[i], NULL);
     free(thread);
 }
+*/
---
+++ assignment4/student/mandelbrot_set_par.d
@@ -0,0 +1,4 @@
+student/mandelbrot_set_par.o: student/mandelbrot_set_par.c \
+ /u/halle/chenyua/home_at/Downloads/SS16-17/ParallelProgramming/parallel_programming_exercises/assignment4/mandelbrot_set.h
+
+/u/halle/chenyua/home_at/Downloads/SS16-17/ParallelProgramming/parallel_programming_exercises/assignment4/mandelbrot_set.h:
